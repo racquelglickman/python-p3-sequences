25734ced3bfa3f24ad26b258c883df2b48e0fa4e
Racquel Glickman <racquel.glickman@gmail.com>
2023-05-01 17:25:12 -0400

    completes lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:  
+        sequence = [0, 1]
+        i = 2
+        while i < length:
+            sequence.append(sequence[i-1] + sequence[i-2])
+            i = i + 1
+        print(sequence)
+     
+    
+
+
